a42d9066bb328b32fa2ed6d2ad5f42b25295a053
FerAma20 <ferama22@gmail.com>
2024-11-02 03:49:51 -0600

    Image gallery file added

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import ItemsData from '../assets/galleryData.json'
 
 export const Gallery=(props)=> {
   return (
@@ -16,7 +17,7 @@ export const Gallery=(props)=> {
       <div className="row">
       <Box >
       <ImageList variant="masonry" cols={3} gap={8}>
-        {itemData.map((item) => (
+        {ItemsData.itemData.map((item) => (
           <ImageListItem key={item.img}>
             <img
               srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
@@ -30,38 +31,6 @@ export const Gallery=(props)=> {
     </Box>
       </div>
     </div>
-  </div>
-    
+  </div>   
   );
 }
-
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-    title: 'Bed',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-    title: 'Books',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-    title: 'Sink',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-    title: 'Doors',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-    title: 'Candle',
-  }
-];
