5c0380c4f13e16ef2463a3742e7d7de2d1c78dad
Brayan Armando Yaquián González <bragonya@gmail.com>
2024-11-03 01:01:41 -0800

    Basic colors adapted to mustache style

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -28,18 +28,8 @@ export const Navigation = (props) => {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll">
-                Features
-              </a>
-            </li>
-            <li>
               <a href="#about" className="page-scroll">
                 About
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="page-scroll">
-                Services
               </a>
             </li>
             <li>
@@ -55,11 +45,6 @@ export const Navigation = (props) => {
             <li>
               <a href="#team" className="page-scroll">
                 Team
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="page-scroll">
-                Contact
               </a>
             </li>
           </ul>
